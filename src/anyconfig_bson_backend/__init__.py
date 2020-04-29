ef428b9a6e2de34f82421d9b67a84592be89356b
Satoru SATOH <satoru.satoh@gmail.com>
2020-04-30 01:49:04 +0900

    change: renmae to avoid conflict with pymongo's bson module

--- src/anyconfig_bson_backend/__init__.py
+++ src/anyconfig_bson_backend/__init__.py
@@ -1,7 +1,7 @@
 """anyconfig backend to load and dump BSON data.
 """
 from __future__ import absolute_import
-from .bson import Parser
+from .bson_ import Parser
 
 __version__ = "0.1.0"
 __all__ = ["Parser"]
